9988276cf8184d0fea31e45242c1b867a1d2ce8d
vicstroie <vas7119@nyu.edu>
2025-04-26 16:56:33 -0400

    Puzzle stuff <3

--- Assets/Scripts/PuzzleElementBehavior.cs
+++ Assets/Scripts/PuzzleElementBehavior.cs
@@ -1,30 +1,49 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class PuzzleElementBehavior : MonoBehaviour
 {
 
     [SerializeField] GameObject mouseTracker;
+    [SerializeField] Material filledMaterial;
 
     public int correctPosition;
     public int currentPosition;
+    public int startPosition;
+
+    bool canTurn;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        canTurn = true;
+        currentPosition = startPosition;
+        this.transform.Rotate(0, 0, -90 * startPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(mouseTracker.transform.position.x < this.transform.position.x + 0.5f && mouseTracker.transform.position.x > this.transform.position.x - 0.5f
-            && mouseTracker.transform.position.y < this.transform.position.y + 0.5f && mouseTracker.transform.position.y > this.transform.position.y - 0.5f
-            && Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            this.transform.Rotate(0, 0, -90);
-            currentPosition += 1;
-            if (currentPosition > 3) currentPosition = 0;
-            Debug.Log("turned");
+            if (canTurn && mouseTracker.transform.position.x < this.transform.position.x + 0.5f && mouseTracker.transform.position.x > this.transform.position.x - 0.5f
+            && mouseTracker.transform.position.y < this.transform.position.y + 0.5f && mouseTracker.transform.position.y > this.transform.position.y - 0.5f)
+            {
+                this.transform.Rotate(0, 0, -90);
+                currentPosition += 1;
+                if (currentPosition > 3) currentPosition = 0;
+                Debug.Log("turned");
+            }
         }
+    }
+
+    public void FillSelf()
+    {
+        this.GetComponent<Renderer>().material = filledMaterial;
+    }
+
+    public void DeactivateSelf()
+    {
+        canTurn = false;
     }
 }
--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -5,6 +5,9 @@ public class PuzzleManager : MonoBehaviour
 {
 
     [SerializeField] GameObject[] correctPieces;
+    [SerializeField] GameObject[] allPieces;
+
+    int filledPiece;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,8 +18,41 @@ public class PuzzleManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < correctPieces.Length; i++) { 
-            
+
+        if (Input.GetMouseButtonUp(0)) {
+
+            int correctCount = 0;
+            for (int i = 0; i < correctPieces.Length; i++)
+            {
+                if (correctPieces[i].GetComponent<PuzzleElementBehavior>().currentPosition == correctPieces[i].GetComponent<PuzzleElementBehavior>().correctPosition) {
+                    correctCount++;
+                }
+            }
+            if (correctCount == correctPieces.Length) {
+                DeactivatePieces();
+                FillPieces();
+            }
+        }
+        
+    }
+
+    void FillPieces()
+    {
+        correctPieces[filledPiece].GetComponent<PuzzleElementBehavior>().FillSelf();
+        if (filledPiece < correctPieces.Length - 1)
+        {
+            filledPiece++;
+            Invoke("FillPieces", 0.5f);
         }
     }
+
+    void DeactivatePieces()
+    {
+        for (int i = 0; i < allPieces.Length; i++)
+        {
+            allPieces[i].GetComponent<PuzzleElementBehavior>().DeactivateSelf();
+        }
+    }
+
+
 }
